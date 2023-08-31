f8461ab3f2504385d17e393b6ec12b67fabc8324
tansheetalitaj <tansheetalitaj@gmail.com>
2023-09-01 00:32:06 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Family Tree</title>
+<!-- Css starts here-->
+<link href="family.css" rel="stylesheet" type="text/css" /> 
+
+<!-- Js ends here -->
+
+<!-- Js ends here -->
+
+</head>
+
+<body>
+
+<div class="tree">
+	<ul>
+		<li>
+			<a href="#">Parent</a>
+			<ul>
+				<li>
+					<a href="#">Child</a>
+					<ul>
+						<li>
+							<a href="#">Grand Child</a>
+              					<ul>
+									<li>
+										<a href="#">Great Grand Child</a>
+									</li>
+									<li>
+										<a href="#">Great Grand Child</a>
+									</li>
+									<li>
+										<a href="#">Great Grand Child</a>
+									</li>
+								</ul>
+						</li>
+            			<li>
+							<a href="#">Grand Child</a>
+						</li>
+            			<li>
+							<a href="#">Grand Child</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">Child</a>
+					<ul>
+						<li><a href="#">Grand Child</a></li>
+						<li>
+							<a href="#">Grand Child</a>
+							<ul>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+							</ul>
+						</li>
+						<li><a href="#">Grand Child</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>
+
+
+</body>
+</html>
